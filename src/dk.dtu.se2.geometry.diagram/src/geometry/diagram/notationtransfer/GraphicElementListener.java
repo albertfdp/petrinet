1c10d2d90e810013b285cc1566fb12b2d37fcce9
Mikko Tuulio <the_magnificient_popo@netti.fi>
2013-11-03 12:04:07 +0000

    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e13-groupA@223 2c9cc009-7d77-48f4-a239-25ca0ae51001

--- src/dk.dtu.se2.geometry.diagram/src/geometry/diagram/notationtransfer/GraphicElementListener.java
+++ src/dk.dtu.se2.geometry.diagram/src/geometry/diagram/notationtransfer/GraphicElementListener.java
@@ -7,16 +7,30 @@ import org.eclipse.emf.ecore.util.EContentAdapter;
 import org.eclipse.emf.transaction.NotificationFilter;
 import org.eclipse.gmf.runtime.notation.LayoutConstraint;
 import org.eclipse.gmf.runtime.notation.Node;
+import org.eclipse.gmf.runtime.notation.NotationPackage;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.gmf.runtime.notation.Bounds;
 import org.eclipse.gmf.runtime.notation.Location;
 import org.eclipse.gmf.runtime.notation.NotationFactory;
+
+import geometry.Point;
+import geometry.impl.PointImpl;
+import geometry.impl.InputPointImpl;
+import geometry.impl.ConnectorImpl;
+import geometry.impl.BendPointImpl;
+import geometry.impl.LineImpl;
 
 
 public class GraphicElementListener extends EContentAdapter {
 	
 	private NotificationFilter ModifiedFilter;
 	
+	/**
+	 * @generated NOT
+	 */
+	public GraphicElementListener() {
+		ModifiedFilter = NotificationFilter.createEventTypeFilter(Notification.SET).or(NotificationFilter.createEventTypeFilter(Notification.UNSET)).or(NotificationFilter.createEventTypeFilter(Notification.ADD)).or(NotificationFilter.createEventTypeFilter(Notification.REMOVE));
+	}
 	/**
 	 * @generated NOT
 	 */
@@ -31,11 +45,8 @@ public class GraphicElementListener extends EContentAdapter {
 			ENotificationImpl eNotification = (ENotificationImpl) notification;
 			EObject eNotifier = (EObject) eNotification.getNotifier();
 			
-			if (eNotifier instanceof Node)
-			{
-				View view = (View) eNotifier;
-				updateNodeBounds(view, eNotifier);
-			}
+			if (NotationPackage.eINSTANCE.getLocation().isSuperTypeOf(eNotifier.eClass()))
+				updateNodeBounds(eNotification, eNotifier);
 		}
 	}
 	
@@ -43,15 +54,56 @@ public class GraphicElementListener extends EContentAdapter {
 	/**
 	 * @generated NOT
 	 */
-	private void updateNodeBounds(View view, EObject eNotifier) {
+	private void updateNodeBounds(ENotificationImpl eNotification, EObject eNotifier) {
+		View view = (View) eNotifier.eContainer();
+		EObject modelElement = view.getElement();
+	    updateNodeBounds(view, modelElement);
+	}
+	
+	/**
+	 * @generated NOT
+	 */
+	private void updateNodeBounds(View view, EObject modelElement) {
 		if(view instanceof org.eclipse.gmf.runtime.notation.Node)
 		{
 			org.eclipse.gmf.runtime.notation.Node viewNode = (org.eclipse.gmf.runtime.notation.Node) view;
+			if (modelElement instanceof InputPointImpl)
+			{
+				Object modelNode = (InputPointImpl) modelElement;
+				Location newPosition = getNodePosition(viewNode);
+				if(modelNode instanceof Point)
+				{
+					modelNode = (Point) modelNode;
+					((Point) modelNode).setXLocation(newPosition.getX());
+					((Point) modelNode).setYLocation(newPosition.getY());
+				}
+			}
 			
-			Location newPosition = getNodePosition(viewNode);
+			else if (modelElement instanceof BendPointImpl)
+			{
+				Object modelNode = (BendPointImpl) modelElement;
+				Location newPosition = getNodePosition(viewNode);
+				if(modelNode instanceof Point)
+				{
+					modelNode = (Point) modelNode;
+					((Point) modelNode).setXLocation(newPosition.getX());
+					((Point) modelNode).setYLocation(newPosition.getY());
+				}
+			}
+			
+			else if (modelElement instanceof ConnectorImpl)
+			{
+				Object modelNode = (ConnectorImpl) modelElement;
+				Location newPosition = getNodePosition(viewNode);
+				if(modelNode instanceof Point)
+				{
+					modelNode = (Point) modelNode;
+					((Point) modelNode).setXLocation(newPosition.getX());
+					((Point) modelNode).setYLocation(newPosition.getY());
+				}
+			}
 		}
 	}
-	
 	
 	/**
 	 * @generated NOT
