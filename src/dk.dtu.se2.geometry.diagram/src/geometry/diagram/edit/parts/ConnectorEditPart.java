7b830ec059392c558dbfa454283fa1cfa2c20e1e
Mikko Tuulio <the_magnificient_popo@netti.fi>
2013-11-03 12:04:37 +0000

    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e13-groupA@224 2c9cc009-7d77-48f4-a239-25ca0ae51001

--- src/dk.dtu.se2.geometry.diagram/src/geometry/diagram/edit/parts/ConnectorEditPart.java
+++ src/dk.dtu.se2.geometry.diagram/src/geometry/diagram/edit/parts/ConnectorEditPart.java
@@ -69,8 +69,7 @@ public class ConnectorEditPart extends ShapeNodeEditPart {
 	 */
 	protected void createDefaultEditPolicies() {
 		super.createDefaultEditPolicies();
-		installEditPolicy(EditPolicyRoles.SEMANTIC_ROLE,
-				new ConnectorItemSemanticEditPolicy());
+		installEditPolicy(EditPolicyRoles.SEMANTIC_ROLE, new ConnectorItemSemanticEditPolicy());
 		installEditPolicy(EditPolicy.LAYOUT_ROLE, createLayoutEditPolicy());
 		// XXX need an SCR to runtime to have another abstract superclass that would let children add reasonable editpolicies
 		// removeEditPolicy(org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles.CONNECTION_HANDLES_ROLE);
