c21cdc8bba0d9dd9eccba4d57ba21e43890a6930
Albert Fernández <albertfdp@gmail.com>
2013-11-08 13:10:43 +0000

    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e13-groupA@263 2c9cc009-7d77-48f4-a239-25ca0ae51001

---
+++ src/dk.dtu.se2.animation/src/dk/dtu/se2/animation/AnimationFactory.java
@@ -0,0 +1,87 @@
+/**
+ */
+package dk.dtu.se2.animation;
+
+import org.eclipse.emf.ecore.EFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Factory</b> for the model.
+ * It provides a create method for each non-abstract class of the model.
+ * <!-- end-user-doc -->
+ * @see dk.dtu.se2.animation.AnimationPackage
+ * @generated
+ */
+public interface AnimationFactory extends EFactory {
+	/**
+	 * The singleton instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	AnimationFactory eINSTANCE = dk.dtu.se2.animation.impl.AnimationFactoryImpl.init();
+
+	/**
+	 * Returns a new object of class '<em>Animation</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Animation</em>'.
+	 * @generated
+	 */
+	Animation createAnimation();
+
+	/**
+	 * Returns a new object of class '<em>Move</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Move</em>'.
+	 * @generated
+	 */
+	Move createMove();
+
+	/**
+	 * Returns a new object of class '<em>Show</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Show</em>'.
+	 * @generated
+	 */
+	Show createShow();
+
+	/**
+	 * Returns a new object of class '<em>Hide</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Hide</em>'.
+	 * @generated
+	 */
+	Hide createHide();
+
+	/**
+	 * Returns a new object of class '<em>Stop</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Stop</em>'.
+	 * @generated
+	 */
+	Stop createStop();
+
+	/**
+	 * Returns a new object of class '<em>Sequence</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Sequence</em>'.
+	 * @generated
+	 */
+	Sequence createSequence();
+
+	/**
+	 * Returns the package supported by this factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the package supported by this factory.
+	 * @generated
+	 */
+	AnimationPackage getAnimationPackage();
+
+} //AnimationFactory
---
+++ src/dk.dtu.se2.animation/src/dk/dtu/se2/animation/impl/HideImpl.java
@@ -0,0 +1,39 @@
+/**
+ */
+package dk.dtu.se2.animation.impl;
+
+import dk.dtu.se2.animation.AnimationPackage;
+import dk.dtu.se2.animation.Hide;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Hide</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class HideImpl extends AnimationImpl implements Hide {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected HideImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return AnimationPackage.Literals.HIDE;
+	}
+
+} //HideImpl
