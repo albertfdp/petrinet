cc2a1d0c14f190ea224811d0cd6827f29eabc943
Albert Fernández <albertfdp@gmail.com>
2013-11-10 10:22:03 +0000

    Updated dependencies
    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e13-groupA@285 2c9cc009-7d77-48f4-a239-25ca0ae51001

--- src/dk.dtu.se2.simulator.petrinet/src/dk/dtu/se2/simulator/petrinet/runtime/RTPlace.java
+++ src/dk.dtu.se2.simulator.petrinet/src/dk/dtu/se2/simulator/petrinet/runtime/RTPlace.java
@@ -1,5 +1,29 @@
 package dk.dtu.se2.simulator.petrinet.runtime;
 
+import java.util.ArrayList;
+
+import dk.dtu.se2.petrinet.Place;
+import dk.dtu.se2.petrinet.Token;
+
 public class RTPlace {
 
+	private Place place;
+	private ArrayList<RToken> tokens;
+	
+	public RTPlace (Place place) {
+		this.place = place;
+		tokens = new ArrayList<RToken>();
+				
+	}
+
+	public ArrayList<RToken> getTokens() {
+		return tokens;
+	}
+
+	public void setTokens(ArrayList<RToken> tokens) {
+		this.tokens = tokens;
+	}
+	
+	
+	
 }
