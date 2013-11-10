9b55a1ec00f5f53f048b6405638f33fec8ec89ed
Thibaud Robelain <thibaud.robelain@insa-lyon.fr>
2013-11-10 11:51:22 +0000

    Petrinet prototype
    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e13-groupA@296 2c9cc009-7d77-48f4-a239-25ca0ae51001

--- src/dk.dtu.se2.simulator.petrinet/src/dk/dtu/se2/simulator/petrinet/PetriNetEngine.java
+++ src/dk.dtu.se2.simulator.petrinet/src/dk/dtu/se2/simulator/petrinet/PetriNetEngine.java
@@ -2,42 +2,120 @@ package dk.dtu.se2.simulator.petrinet;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Iterator;
 
+import org.pnml.tools.epnk.pnmlcoremodel.Arc;
+import org.pnml.tools.epnk.pnmlcoremodel.Object;
 import org.pnml.tools.epnk.pnmlcoremodel.PetriNetDoc;
 import org.pnml.tools.epnk.pnmlcoremodel.Transition;
 
-import dk.dtu.se2.animation.Animation;
-import dk.dtu.se2.petrinet.ExtendedPetriNet;
 import dk.dtu.se2.petrinet.Place;
-import dk.dtu.se2.petrinet.Token;
+import dk.dtu.se2.simulator.petrinet.runtime.RTToken;
 
 /*
- * @author: Thibaud
+ * @author Thibaud
+ * @generated NOT
  */
 public class PetriNetEngine {
 	
-	private HashMap<Place, ArrayList<Token>> marking = new HashMap<Place, ArrayList<Token>>();
+	private HashMap<Place, ArrayList<RTToken>> marking = new HashMap<Place, ArrayList<RTToken>>();
 	private ArrayList<Transition> transitions;
 	
-	public List<Animation> init (PetriNetDoc petrinet) {
-		return null;
+	public ArrayList<String> init (PetriNetDoc petrinet) {
+		
+		Iterator<Object> iterator = petrinet.getNet().get(0).getPage().get(0).getObject().iterator();
+		ArrayList<String> animations = new ArrayList<String>();
+		
+		while (iterator.hasNext()) {
+			Object item = iterator.next();
+			if (item instanceof Transition) {
+				Transition newTransition = (Transition)item;
+				this.transitions.add(newTransition);
+			} else if (item instanceof Place) {
+				Place newPlace = (Place)item;
+				ArrayList<RTToken> tokens = new ArrayList<RTToken>();
+				for (int i = 0; i < newPlace.getTokens().size(); i++) {
+					RTToken runtimeToken = new RTToken();
+					tokens.add(runtimeToken);
+					animations.add(newPlace.getGeometryLabel().getText());
+				}
+				marking.put(newPlace, tokens);
+			}
+		}
+		
+		return animations;
 	}
 	
-	public void fireTransitions() {
+	public ArrayList<String> fireTransitions() {
+		ArrayList<String> animations = new ArrayList<String>();
+		
+		for (Transition transition : transitions) {
+			if (isTransitionEnabled(transition)) {
+				ArrayList<String> newAnimations = fireTransition(transition);
+				animations.addAll(newAnimations);
+			}
+		}
+		
+		return animations;
+	}
+	
+	public void markTokenAsFinished(Place place) {
+		for (RTToken runtimeToken : marking.get(place) ) {
+			if (!runtimeToken.isFinished()) {
+				runtimeToken.setFinished(true);
+				break;
+			}
+		}
+	}
+	
+	public void createToken(Place place) {
+		RTToken newToken = new RTToken();
+		marking.get(place).add(newToken);
 		
 	}
-	
-	public void setMarker(Token token, Place place) {}
-	
-	public void createToken(Place place) {}
 
-	
+	private ArrayList<String> fireTransition(Transition transition) {
+		ArrayList<String> transitionAnimations = new ArrayList<String>();
+		
+		for (Arc arc: transition.getIn()) {
+			if (arc.getSource() instanceof Place) {
+				Place place = (Place)arc.getSource();
+				for (RTToken runtimeToken : marking.get(place)) {
+					if (runtimeToken.isFinished()) {
+						marking.get(place).remove(runtimeToken);
+						break;
+					}
+				}
+			}
+		}
+		for (Arc arc: transition.getOut()) {
+			if (arc.getTarget() instanceof Place) {
+				Place place = (Place)arc.getTarget();
+				RTToken newToken = new RTToken();
+				marking.get(place).add(newToken);
+				transitionAnimations.add(place.getGeometryLabel().getText());
+			}
+		}
+		
+		return transitionAnimations;
+	}
 	
 	private boolean isTransitionEnabled (Transition transition) {
-		boolean isEnabled = false;
 		
-		
-		return isEnabled;
+		for (Arc arc : transition.getIn()) {
+			boolean atLeastOneToken = false;
+			if (arc.getSource() instanceof Place) {
+				Place place = (Place)arc.getSource();
+				for (RTToken runtimeToken : marking.get(place)) {
+					if (runtimeToken.isFinished()) {
+						atLeastOneToken = true;
+					}
+				}
+			}
+			if (!atLeastOneToken) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
