2956ea3dfd2615da31f2b860cfc8c9b92b6d091e
Thibaud Robelain <thibaud.robelain@insa-lyon.fr>
2013-11-10 10:50:51 +0000

    RTTransition
    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e13-groupA@294 2c9cc009-7d77-48f4-a239-25ca0ae51001

--- src/dk.dtu.se2.simulator.petrinet/src/dk/dtu/se2/simulator/petrinet/runtime/RTTransition.java
+++ src/dk.dtu.se2.simulator.petrinet/src/dk/dtu/se2/simulator/petrinet/runtime/RTTransition.java
@@ -1,5 +1,57 @@
 package dk.dtu.se2.simulator.petrinet.runtime;
 
-public class RTTransition {
+import java.util.ArrayList;
 
+import org.pnml.tools.epnk.pnmlcoremodel.Arc;
+import org.pnml.tools.epnk.pnmlcoremodel.Transition;
+
+import dk.dtu.se2.petrinet.Place;
+
+/*
+ * @author Thibaud
+ * @generated NOT
+ * 
+ * 
+ */
+
+
+
+public class RTTransition {
+	
+	ArrayList<RTPlace> sources;
+	ArrayList<RTPlace> targets;
+	ArrayList<String> transitionFiredAnimations;
+	
+	public RTTransition(Transition transition) {
+		
+		this.transitionFiredAnimations = new ArrayList<String>();
+		
+		for(Arc incomingArc : transition.getIn()) {
+			if (incomingArc instanceof Place) {
+				Place incomingPlace = (Place)incomingArc.getSource();
+				RTPlace runtimePlace = new RTPlace(incomingPlace);
+				sources.add(runtimePlace);
+			}
+		}
+		for(Arc outgoingArc : transition.getOut()) {
+			if (outgoingArc instanceof Place) {
+				Place outgoingPlace = (Place)outgoingArc.getTarget();
+				RTPlace runtimePlace = new RTPlace(outgoingPlace);
+				targets.add(runtimePlace);
+				transitionFiredAnimations
+			}
+		}
+		
+		
+		
+		
+	}
+
+	public fireTransition() {
+		
+	}
+	
+	private boolean isTransitionEnabled() {
+		
+	}
 }
