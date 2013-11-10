dc61c60cc955d8a1081efd82b8e076b2078fabfa
Monica Coman <monicaandreea.coman@gmail.com>
2013-11-10 10:29:00 +0000

    //configuration editor
    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e13-groupA@287 2c9cc009-7d77-48f4-a239-25ca0ae51001

---
+++ src/dk.dtu.se2.configuration/src/configuration/impl/ConfigurationImpl.java
@@ -0,0 +1,220 @@
+/**
+ */
+package configuration.impl;
+
+import configuration.Configuration;
+import configuration.ConfigurationPackage;
+
+import geometry.Geometry;
+
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+import org.pnml.tools.epnk.pnmlcoremodel.PetriNetDoc;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Configuration</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link configuration.impl.deleted.ConfigurationImpl#getPetrinet <em>Petrinet</em>}</li>
+ *   <li>{@link configuration.impl.deleted.ConfigurationImpl#getGeometry <em>Geometry</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class ConfigurationImpl extends MinimalEObjectImpl.Container implements Configuration {
+	/**
+	 * The cached value of the '{@link #getPetrinet() <em>Petrinet</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getPetrinet()
+	 * @generated
+	 * @ordered
+	 */
+	protected PetriNetDoc petrinet;
+
+	/**
+	 * The cached value of the '{@link #getGeometry() <em>Geometry</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getGeometry()
+	 * @generated
+	 * @ordered
+	 */
+	protected Geometry geometry;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ConfigurationImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return ConfigurationPackage.Literals.CONFIGURATION;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public PetriNetDoc getPetrinet() {
+		if (petrinet != null && petrinet.eIsProxy()) {
+			InternalEObject oldPetrinet = (InternalEObject)petrinet;
+			petrinet = (PetriNetDoc)eResolveProxy(oldPetrinet);
+			if (petrinet != oldPetrinet) {
+				if (eNotificationRequired())
+					eNotify(new ENotificationImpl(this, Notification.RESOLVE, ConfigurationPackage.CONFIGURATION__PETRINET, oldPetrinet, petrinet));
+			}
+		}
+		return petrinet;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public PetriNetDoc basicGetPetrinet() {
+		return petrinet;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setPetrinet(PetriNetDoc newPetrinet) {
+		PetriNetDoc oldPetrinet = petrinet;
+		petrinet = newPetrinet;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, ConfigurationPackage.CONFIGURATION__PETRINET, oldPetrinet, petrinet));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Geometry getGeometry() {
+		if (geometry != null && geometry.eIsProxy()) {
+			InternalEObject oldGeometry = (InternalEObject)geometry;
+			geometry = (Geometry)eResolveProxy(oldGeometry);
+			if (geometry != oldGeometry) {
+				if (eNotificationRequired())
+					eNotify(new ENotificationImpl(this, Notification.RESOLVE, ConfigurationPackage.CONFIGURATION__GEOMETRY, oldGeometry, geometry));
+			}
+		}
+		return geometry;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Geometry basicGetGeometry() {
+		return geometry;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setGeometry(Geometry newGeometry) {
+		Geometry oldGeometry = geometry;
+		geometry = newGeometry;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, ConfigurationPackage.CONFIGURATION__GEOMETRY, oldGeometry, geometry));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case ConfigurationPackage.CONFIGURATION__PETRINET:
+				if (resolve) return getPetrinet();
+				return basicGetPetrinet();
+			case ConfigurationPackage.CONFIGURATION__GEOMETRY:
+				if (resolve) return getGeometry();
+				return basicGetGeometry();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case ConfigurationPackage.CONFIGURATION__PETRINET:
+				setPetrinet((PetriNetDoc)newValue);
+				return;
+			case ConfigurationPackage.CONFIGURATION__GEOMETRY:
+				setGeometry((Geometry)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case ConfigurationPackage.CONFIGURATION__PETRINET:
+				setPetrinet((PetriNetDoc)null);
+				return;
+			case ConfigurationPackage.CONFIGURATION__GEOMETRY:
+				setGeometry((Geometry)null);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case ConfigurationPackage.CONFIGURATION__PETRINET:
+				return petrinet != null;
+			case ConfigurationPackage.CONFIGURATION__GEOMETRY:
+				return geometry != null;
+		}
+		return super.eIsSet(featureID);
+	}
+
+} //ConfigurationImpl
