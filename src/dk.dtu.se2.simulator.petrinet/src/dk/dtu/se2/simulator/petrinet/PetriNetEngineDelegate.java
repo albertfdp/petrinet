5e1fc54b44feb6b4933135cf85530f0f3428408f
Thibaud Robelain <thibaud.robelain@insa-lyon.fr>
2013-11-29 15:59:03 +0000

    Interface for communication with the simulator
    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e13-groupA@400 2c9cc009-7d77-48f4-a239-25ca0ae51001

---
+++ src/dk.dtu.se2.simulator.petrinet/src/dk/dtu/se2/simulator/petrinet/PetriNetEngineDelegate.java
@@ -0,0 +1,19 @@
+package dk.dtu.se2.simulator.petrinet;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Thibaud Robelain
+ * @generated NOT
+ *
+ */
+public interface PetriNetEngineDelegate {
+
+	/**
+	 * 
+	 * @param geometryLabels
+	 */
+	public void destroyRepresentations(ArrayList<String> geometryLabels);
+	
+}
