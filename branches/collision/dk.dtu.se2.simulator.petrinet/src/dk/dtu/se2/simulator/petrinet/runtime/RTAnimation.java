228a65a1f6edbde9d02bcf8ffd66fd045f2c890e
Thibaud Robelain <thibaud.robelain@insa-lyon.fr>
2013-12-11 09:24:11 +0000

    Many tokens on the same line
    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e13-groupA@524 2c9cc009-7d77-48f4-a239-25ca0ae51001

---
+++ branches/collision/dk.dtu.se2.simulator.petrinet/src/dk/dtu/se2/simulator/petrinet/runtime/RTAnimation.java
@@ -0,0 +1,98 @@
+package dk.dtu.se2.simulator.petrinet.runtime;
+
+import dk.dtu.se2.animation.Animation;
+
+public class RTAnimation {
+	
+	/* The place unique id */
+	private String id;
+	
+	/* The geometry label of that place */
+	private String geometryLabel;
+	
+	/* The animation to run */
+	private Animation animation;
+	
+	private boolean destroy;
+	
+	public RTAnimation() {}
+	
+	public RTAnimation(String id, String geometryLabel, Animation animation, boolean destroy) {
+		super();
+		this.id = id;
+		this.geometryLabel = geometryLabel;
+		this.animation = animation;
+		this.destroy = destroy;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getGeometryLabel() {
+		return geometryLabel;
+	}
+
+	public void setGeometryLabel(String geometryLabel) {
+		this.geometryLabel = geometryLabel;
+	}
+
+	public Animation getAnimation() {
+		return animation;
+	}
+
+	public void setAnimation(Animation animation) {
+		this.animation = animation;
+	}
+	
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RTAnimation other = (RTAnimation) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "RTAnimation [geometryLabel=" + geometryLabel + ", animation="
+				+ animation + "]";
+	}
+
+	public boolean isDestroy() {
+		return destroy;
+	}
+
+	public void setDestroy(boolean destroy) {
+		this.destroy = destroy;
+	}
+
+	
+	
+}
