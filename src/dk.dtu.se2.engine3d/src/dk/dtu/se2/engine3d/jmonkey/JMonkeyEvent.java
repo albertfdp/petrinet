189b57ddaa7addb9fba1d03d3e5856d3b7123f4f
Thibaud Robelain <thibaud.robelain@insa-lyon.fr>
2013-12-11 20:44:11 +0000

    Cleanup engine 3D merge from branch
    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e13-groupA@530 2c9cc009-7d77-48f4-a239-25ca0ae51001

--- src/dk.dtu.se2.engine3d/src/dk/dtu/se2/engine3d/jmonkey/JMonkeyEvent.java
+++ src/dk.dtu.se2.engine3d/src/dk/dtu/se2/engine3d/jmonkey/JMonkeyEvent.java
@@ -1,17 +1,38 @@
 package dk.dtu.se2.engine3d.jmonkey;
 
 
+import com.jme3.animation.LoopMode;
+import com.jme3.cinematic.MotionPath;
+import com.jme3.cinematic.events.MotionEvent;
+import com.jme3.scene.Spatial;
+
+
+
+
 /**
  * @author Monica
  *
  */
 public class JMonkeyEvent {
 	
+	
+	//An id for the JMonkeyEvent
+	private String id;
+	
+	//The geometry label
 	private String geometryLabel;
-		
+	
 	public JMonkeyEvent(String geometryLabel) {
 		super();
 		this.geometryLabel = geometryLabel;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
 	}
 
 	public String getGeometryLabel() {
@@ -22,14 +43,26 @@ public class JMonkeyEvent {
 		this.geometryLabel = geometryLabel;
 	}
 
-	public static JMonkeyEvent eventCopy(JMonkeyEvent baseObject) {
-		JMonkeyEvent copy = null;
-		if (baseObject instanceof JMonkeyMove) {
-			copy = new JMonkeyMove(baseObject.getGeometryLabel(), ((JMonkeyMove) baseObject).getMotionEvent(), ((JMonkeyMove) baseObject).getEngine3D());
-		} else if (baseObject instanceof JMonkeyAppear) {
-			copy = new JMonkeyAppear(baseObject.getGeometryLabel(), ((JMonkeyAppear) baseObject).getAppearanceLabel());
-		}
-		
+	public static JMonkeyEvent appearCopy(JMonkeyEvent baseObject) {
+		JMonkeyAppear copy = new JMonkeyAppear(baseObject.getGeometryLabel(), ((JMonkeyAppear) baseObject).getAppearanceLabel());
 		return copy;
+		
 	}
+	
+	public static JMonkeyEvent moveEvent(JMonkeyEvent baseObject, MotionPath newPath, Spatial spatialToCopy) {
+		MotionEvent oldMotionEvent = ((JMonkeyMove) baseObject).getMotionEvent();
+		
+		//Cloning the old motion event 
+		MotionEvent newMotionEvent = new MotionEvent(spatialToCopy, newPath, 10, LoopMode.DontLoop);
+		newMotionEvent.setSpeed(oldMotionEvent.getSpeed());
+		newMotionEvent.setDirectionType(MotionEvent.Direction.Path);
+		
+		//Copying the JMonkeyMove
+		JMonkeyMove copy = new JMonkeyMove(baseObject.getGeometryLabel(), newMotionEvent, ((JMonkeyMove) baseObject).getEngine3D());
+		copy.setEndWayPoint( newPath.getWayPoint( newPath.getNbWayPoints()-1) );
+		newMotionEvent.addListener(copy); // add the JMonkeyEngine as a listener to all motion events	
+
+		return copy;
+		
+	}	
 }
