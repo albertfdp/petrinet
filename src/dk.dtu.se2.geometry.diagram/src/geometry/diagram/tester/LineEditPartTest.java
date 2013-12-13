c6a74dd627b5b596a13807074f88b772fb124e2a
Mikko Tuulio <the_magnificient_popo@netti.fi>
2013-12-13 13:27:26 +0000

    Removed error
    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e13-groupA@545 2c9cc009-7d77-48f4-a239-25ca0ae51001

--- src/dk.dtu.se2.geometry.diagram/src/geometry/diagram/tester/LineEditPartTest.java
+++ src/dk.dtu.se2.geometry.diagram/src/geometry/diagram/tester/LineEditPartTest.java
@@ -1,23 +1,6 @@
 package geometry.diagram.tester;
 
 import static org.junit.Assert.*;
-
-import org.eclipse.draw2d.Connection;
-import org.eclipse.draw2d.geometry.PointList;
-import org.eclipse.gmf.runtime.notation.Edge;
-import org.eclipse.gmf.runtime.notation.View;
-import org.eclipse.gmf.runtime.notation.NotationFactory;
-import org.eclipse.gmf.runtime.diagram.core.view.factories.ViewFactory;
-import org.eclipse.gmf.runtime.diagram.ui.view.factories.*;
-import org.eclipse.gmf.runtime.diagram.core.providers.*;
-
-import geometry.GeometryPackage;
-import geometry.diagram.edit.parts.GeometryEditPart;
-import geometry.diagram.edit.parts.GeometryEditPartFactory;
-import geometry.diagram.edit.parts.LineEditPart;
-import geometry.diagram.edit.parts.LineEditPart.LineFigure;
-import geometry.diagram.providers.GeometryEditPartProvider;
-
 import org.junit.Test;
 
 public class LineEditPartTest {
@@ -29,21 +12,7 @@ public class LineEditPartTest {
 	@Test
 	public void testCalculateCatmullRomPoints() {
 		
-		LineFigure lineFigure = GeometryEditPartFactory.
-		
-		PointList testPoints = new PointList();
-		
-		PointList straightLinePoints = new PointList();
-		testPoints.addPoint(5, 5);
-		testPoints.addPoint(10, 10);
-		
-		straightLinePoints.addPoint(5, 5);
-		straightLinePoints.addPoint(10, 10);
-		
-		
-		//straightLinePoints = lineFigure.calculateCatmullromPointList(testPoints, 5);
-		
-		assertEquals("moi", 6, straightLinePoints.getPoint(1).x());
+		assertEquals("They have different number", 6, 5);
 		
 	}
 
