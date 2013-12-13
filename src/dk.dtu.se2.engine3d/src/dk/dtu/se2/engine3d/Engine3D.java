6f3ccdaa10c9633cc2b445ef5335eff89e750226
Thibaud Robelain <thibaud.robelain@insa-lyon.fr>
2013-12-13 14:23:19 +0000

    Javadoc
    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e13-groupA@552 2c9cc009-7d77-48f4-a239-25ca0ae51001

--- src/dk.dtu.se2.engine3d/src/dk/dtu/se2/engine3d/Engine3D.java
+++ src/dk.dtu.se2.engine3d/src/dk/dtu/se2/engine3d/Engine3D.java
@@ -8,16 +8,40 @@ import dk.dtu.se2.simulator.petrinet.runtime.RTAnimation;
 
 public interface Engine3D {
 	
+	/**
+	 * Initializes the 3D Engine with the given parameters
+	 * @param geometry: The geometry of the Petri net
+	 * @param appearance: The appearance containing each 3D Model and texture
+	 * @param animations: All the possible animations in the Petri net on each place (Move, Appear)
+	 */
 	public void init (Geometry geometry, Appearance appearance, List<RTAnimation> animations);
 	
+	/**
+	 * Sets the listener for this 3D Engine
+	 * @param listener
+	 */
 	public void setEngine3DListener(Engine3DListener listener);
 	
+	/**
+	 * Adds the given animations to the queue of animations to render in the 3D Engine
+	 * @param animations
+	 */
 	public void addToAnimationQueue(List<RTAnimation> animations);
 	
+	/**
+	 * Starts the engine
+	 */
 	public void startEngine();
 	
-	public void destroyRepresentation(String geometryLabel);
+	/**
+	 * Removes a given token from the 3D Engine visualization
+	 * @param id: The id of the token to remove
+	 */
+	public void destroyRepresentation(String id);
 	
+	/**
+	 * Resets the 3D Engine to its initial state
+	 */
 	public void reset();
 
 }
